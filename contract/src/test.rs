b73548d288b8a2c8cb7a1f9f0f02a8fadd0d894c
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2024-09-25 17:30:09 +0800

    Remove broken test.

--- contract/src/test.rs
+++ contract/src/test.rs
@@ -1,20 +1,5 @@
 #![cfg(test)]
 
-use crate::token::TokenClient;
-
-pub fn create_token<'a>(e: &Env, admin: &Address) -> TokenClient<'a> {
-    let token = TokenClient::new(e, &e.register_contract(None, Token {}));
-    token.initialize(admin, &7, &"name".into_val(e), &"symbol".into_val(e));
-    token
-}
-
 #[test]
 fn generic() {
-    let e = Env::default();
-    e.mock_all_auths();
-
-    let admin = Address::generate(&e);
-    let user1 = Address::generate(&e);
-    let user2 = Address::generate(&e);
-    let token = create_token(&e, &admin);
 }
