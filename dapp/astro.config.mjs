8b6ce7416850176c6f57245eb9d8f57f8fc93ca5
Daryl <dary0014@e.ntu.edu.sg>
2024-10-16 02:12:12 +0800

    add redirect to separate page for each Prediction contract (WIP)

--- dapp/astro.config.mjs
+++ dapp/astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-      plugins: [basicSsl()],
-      server: {
-        https: true,
-      },
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
     },
-  })
+  },
+  routes: [
+    '/predictions',
+  ],
+  integrations: [react()]
+})
