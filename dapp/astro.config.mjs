0c139d32b2dc50f4b0461dbb1fc692de02955b24
Daryl <dary0014@e.ntu.edu.sg>
2024-10-16 21:36:52 +0800

    working demo for Carousel effect in Astro

--- dapp/astro.config.mjs
+++ dapp/astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
     '/local',
     '/display',
     '/carousel',
+    '/App',
   ],
   integrations: [react(), tailwind()]
 })
--- dapp/src/components/SimpleSlider.jsx
+++ dapp/src/components/SimpleSlider.jsx
@@ -1,34 +1,63 @@
 import React from "react";
-import Slider from "react-slick";
+import Slider from '@ant-design/react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import marketData from "../data/markets.json"
+import CardCarousel from '../components/CardCarousel.astro';
 
-export default function SimpleSlider() {
+const SimpleSlider = () => {
   var settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    speed: 400,
+    slidesToShow: 3,
+    slidesToScroll: 3
   };
   return (
-    <Slider {...settings}>
-      <div>
-        <h3>1</h3>
+        <div className='h-screen bg-slate-700 my-10'>
+      <div className='w-4/8 mx-8 my-4 '>
+      <Slider {...settings}>
+    {
+            marketData.map((marketData, index) => {
+            return (
+                <div style="margin: 0 20px;" className="bg-slate-700 p-2">
+                <div key={index} className='bg-white p-2 rounded-lg h-screen'>
+                  <div>
+                    <img src={marketData.imageUrl} />
+                  </div>
+                  <div>
+                      <p className='font-semibold text-black text-center'>
+                        {marketData.title} 
+                    </p>
+                  </div>
+                  <div className='p-6 bg-blue-600 p-2'>
+                    <p className='font-semibold text-white text-center'>
+                        {marketData.description}
+                        </p>
+                    </div>
+                    <div className='p-6 bg-gray-500 p-2'>
+                    <ul className='font-semibold text-white text-center unstyled'>
+                        {marketData.betOptions.map((option, index) => (
+                        <li key={index}>
+                          <h3>{option}</h3>
+                          <div className='p-6 bg-orange-400 p-2 rounded-lg'>
+                          <p className='font-semibold text-white text-center'>
+                              ({marketData.betPercentage[index]}%)
+                              </p>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            );
+            })
+        }  
+        </Slider>
       </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
+    </div>    
   );
 }
+
+export default SimpleSlider;
