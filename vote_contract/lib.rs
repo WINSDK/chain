d32a0f0418ce436f4944029a15103a6f5c92a004
Charles Chan <charles.chan99@gmail.com>
2024-11-06 20:43:55 +0800

    changes to Admin, Poll structs, contract deployable

--- vote_contract/lib.rs
+++ vote_contract/lib.rs
@@ -1,20 +1,5 @@
 #![no_std]
-use soroban_sdk::{Address, contract, contracttype, contractimpl, Env, log, String, Symbol, symbol_short, token};
-
-#[contracttype]
-#[derive(Clone)]
-// struct for this prediction contract's metadata, uses const POLL
-pub struct Poll {
-    pub is_init: u64,
-    pub winner: Symbol,
-    pub start_time: u64,
-    pub end_time: u64,
-    pub opt1: u64,
-    pub opt2: u64,
-    pub total: u64,
-}
-
-const POLL: Symbol = symbol_short!("POLL");
+use soroban_sdk::{Address, contract, contracttype, contractimpl, Env, log, Symbol, symbol_short, token};
 
 #[contracttype]
 // struct for mapping user Address to their votes for this Contract
@@ -22,13 +7,28 @@ const POLL: Symbol = symbol_short!("POLL");
 pub enum Registry {
     Record(Address),
     Admin(Address),
+    Prediction,
 }
+
+#[contracttype]
+#[derive(Clone)]
+pub struct PredictionRecord {
+    pub has_init: u64,
+    pub start_t: u64,
+    pub end_t: u64,
+    pub opt_1: u64,
+    pub opt_2: u64,
+    pub total: u64,
+    pub winner: Symbol,
+}
+
+const PRED: Symbol = symbol_short!("PRED");
 
 #[contracttype]
 #[derive(Clone)]
 // struct for admin address
 pub struct AdminRecord {
-    pub is_init: u64,
+    pub has_init: u64,
 
 }
 
@@ -52,63 +52,42 @@ pub struct VoteContract;
 impl VoteContract {
 
     // called after contract deployment by the admin to enable voting for this contract
-    // should be called only once
-    pub fn init(
-        env: Env,
-        admin: Address,
-        duration: u64,
+    // should be called only once, adds the admin init to storage
+    pub fn init(env: Env, admin: Address, duration: u64,
     ) {
         // check if init() has already been called for this contract
-        let mut poll = Self::view_poll(env.clone());
-        if poll.is_init == 1 {
+        let mut pred = Self::view_predictions(env.clone());
+        if pred.has_init == 1 {
             panic!("Already init");
         }
-        else if poll.start_time != 0 {
+        else if pred.start_t != 0 {
             panic!("Already init");
         }
         else {
             // add admin address to registry
             admin.require_auth();
             let admin_rec = AdminRecord {
-                is_init: 1,
+                has_init: 1,
             };
             env.storage().instance().set(&Registry::Admin(admin), &admin_rec);
             // add contract start and end time
-            poll.start_time = env.ledger().timestamp();
-            poll.end_time = env.ledger().timestamp() + duration;
+            pred.start_t = env.ledger().timestamp();
+            pred.end_t = env.ledger().timestamp() + duration;
             // finally assert that the contract has been init, set the rest of the values
-            poll.is_init = 1;
-            poll.winner = symbol_short!("none");
-            poll.opt1 = 0;
-            poll.opt2 = 0;
-            poll.total = 0;
-            env.storage().instance().set(&POLL, &poll);
+            pred.has_init = 1;
+            pred.winner = NONE;
+            pred.opt_1 = 0;
+            pred.opt_2 = 0;
+            pred.total = 0;
+            env.storage().instance().set(&Registry::Prediction, &pred);
             env.storage().instance().extend_ttl(100, 100);
         }
-    }
-
-    // testing fns for admin init
-    pub fn set_admin(env: Env, admin: Address) {
-        // add admin address to registry
-        admin.require_auth();
-        let admin_rec = AdminRecord {
-            is_init: 1,
-        };
-        env.storage().instance().set(&Registry::Admin(admin.clone()), &admin_rec);
-        env.storage().instance().extend_ttl(100, 100);
-    }
-
-    pub fn get_admin(env: Env, admin: Address) -> AdminRecord {
-        env.storage().instance().get(&Registry::Admin(admin)).unwrap_or(AdminRecord {
-            is_init: 0,
-        })
     }
 
     // function to stake votes for a prediction, in the dapp this must be called along
     // with the payment transaction for the correct amount
     // Note: each user can only stake votes once in this version of the contract
     pub fn record_votes(env: Env, user: Address, selected: Symbol, votes: u64) -> Symbol {
-        // .clone() to be able to use the value again in this fn
         let mut records = Self::view_voter(env.clone(), user.clone());
         user.require_auth();
         let time = env.ledger().timestamp();
@@ -117,19 +96,19 @@ impl VoteContract {
             panic!("Cannot vote");
         }
         else {
-            let mut poll = Self::view_poll(env.clone());
+            let mut pred = Self::view_predictions(env.clone());
             records.selected = selected;
             records.votes = votes;
             records.time = time;
             if records.selected == OPT1 {
-                poll.opt1 = poll.opt1 + votes;
+                pred.opt_1 = pred.opt_1 + votes;
             }
             else if records.selected == OPT2 {
-                poll.opt2 = poll.opt2 + votes;
+                pred.opt_2 = pred.opt_2 + votes;
             }
-            poll.total = poll.total + votes;
+            pred.total = pred.total + votes;
             env.storage().instance().set(&Registry::Record(user), &records);
-            env.storage().instance().set(&POLL, &poll);
+            env.storage().instance().set(&Registry::Prediction, &pred);
             env.storage().instance().extend_ttl(100, 100);
 
             return symbol_short!("Recorded");
@@ -137,15 +116,16 @@ impl VoteContract {
     }
 
     // function to get Poll market values, also provides initial values for the struct
-    pub fn view_poll(env: Env) -> Poll {
-        env.storage().instance().get(&POLL).unwrap_or(Poll {
-            is_init: 0,
-            winner: symbol_short!("none"),
-            start_time: 0,
-            end_time: 0,
-            opt1: 0,
-            opt2: 0,
+    pub fn view_predictions(env: Env) -> PredictionRecord {
+        // let key = Registry::Poll(POLL);
+        env.storage().instance().get(&Registry::Prediction).unwrap_or(PredictionRecord {
+            has_init: 0,
+            start_t: 0,
+            end_t: 0,
+            opt_1: 0,
+            opt_2: 0,
             total: 0,
+            winner: NONE,
         })
     }
     
@@ -159,35 +139,43 @@ impl VoteContract {
         })
     }
 
+    // internal function to view admin
+    pub fn view_admin(env: Env, admin: Address) -> AdminRecord {
+        let key = Registry::Admin(admin.clone());
+        env.storage().instance().get(&key).unwrap_or(AdminRecord {
+            has_init: 0,
+        })
+    }
+
     // function for admins to close the market, specifying a winner
     // input should either be Symbol "OPT1" or Symbol "OPT2", else remains as "none"
-    pub fn close(env: Env, admin: Address, winner: Symbol) -> Poll {
+    pub fn close(env: Env, admin: Address, winner: Symbol) -> PredictionRecord {
         admin.require_auth();
-        let mut poll = Self::view_poll(env.clone());
-        let key = env.storage().instance().get(&Registry::Admin(admin)).unwrap_or(0);
-        if key == 0 {
-            panic!("Invalid admin");
+        let mut pred = Self::view_predictions(env.clone());
+        let admin_cred = Self::view_admin(env.clone(), admin.clone());
+        if admin_cred.has_init == 0 {
+            panic!("Admin invalid");
         }
         else {
-            if poll.winner != NONE {
-                panic!("Poll already closed");
+            if pred.winner != NONE {
+                panic!("Predictions already closed");
             }
 
             if winner.clone() == OPT1 {
-                log!(&env, "Winner OPT1: {}", poll.opt1);
+                log!(&env, "Winner OPT1: {}", pred.opt_1);
             }
             else if winner.clone() == OPT2 {
-                log!(&env, "Winner OPT2: {}", poll.opt2);
+                log!(&env, "Winner OPT2: {}", pred.opt_2);
             }
             else {
                 panic!("Invalid winner");
             }
 
-            poll.is_init = 0;
-            poll.winner = winner;
-            env.storage().instance().set(&POLL, &poll);
+            pred.has_init = 0;
+            pred.winner = winner;
+            env.storage().instance().set(&Registry::Prediction, &pred);
             env.storage().instance().extend_ttl(100, 100);
-            return poll;
+            return pred;
         }
     }
 
@@ -199,14 +187,14 @@ impl VoteContract {
         let key = Registry::Record(user.clone());
         user.require_auth();
 
-        let poll = Self::view_poll(env.clone());
+        let poll = Self::view_predictions(env.clone());
         let user_record = env.storage().instance().get(&key).unwrap_or(0);
         
         // if user has a record and market is open, not valid
         if user_record == 0 {
             panic!("No user record");
         }
-        else if poll.is_init == 0 {
+        else if poll.has_init == 0 {
             panic!("Market not closed");
         }
         else if poll.winner == NONE {
@@ -220,8 +208,8 @@ impl VoteContract {
             // allocate XLM payments from current pool, based on user's vote 
             // proportion of total, since each vote means a certain amount bet
             // formula = voter's full vote amount + proportion of votes from the losing pool
-            let opt1_votes: f64 = poll.opt1 as f64;
-            let opt2_votes: f64 = poll.opt2 as f64;
+            let opt1_votes: f64 = poll.opt_1 as f64;
+            let opt2_votes: f64 = poll.opt_2 as f64;
             let user_votes: f64 = records.votes as f64;
 
             if poll.winner == records.selected {
